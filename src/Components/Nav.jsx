ffd5af68258718779c06d7c5bb5ac4a2dd5817ac
ayush ranjan <ayushranjan656@gmail.com>
2023-07-06 16:54:58 +0530

    'sixth'

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,166 +6,168 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Button from '@mui/material/Button';
 
 const Nav = () => {
-    const [x, setX] = useState(window.innerWidth);
-    const [showMenu, setShowMenu] = useState(false);
-    const [isBurgerMoved, setIsBurgerMoved] = useState(false);
+  const [x, setX] = useState(window.innerWidth);
+  const [showMenu, setShowMenu] = useState(false);
+  const [isBurgerMoved, setIsBurgerMoved] = useState(false);
 
 
-    useEffect(() => {
-        const handleResize = () => {
-            setX(window.innerWidth);
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      setX(window.innerWidth);
+    };
 
-        window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-    const toggleMenu = () => {
-                setShowMenu(!showMenu);
-                setIsBurgerMoved(!isBurgerMoved);
-            };
-            const burgerClassName = isBurgerMoved ? 'burger unto' : 'burger';
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    setIsBurgerMoved(!isBurgerMoved);
+  };
+  const burgerClassName = isBurgerMoved ? 'burger unto' : 'burger';
 
-    const loc = useLocation();
-//   useEffect(()=>{
-//     
+  const loc = useLocation();
+  //   useEffect(()=>{
+  //     
 
-//   })
-  
-    return (
-        <>
-            <div className="nav">
-                {x < 500  ? (
-                    <div className={burgerClassName} onClick={toggleMenu}>
-                        <Button variant="outlined">
-                            <MenuIcon style={{ color: 'white' }} />
-                        </Button>
-                    </div>
-                ) : null}
-                {showMenu && x < 500 ? (
-                    <div className="menu">
-                        <NavLink exact to="/" className="bar">
-                            <p>Home</p>
-                        </NavLink>
-                        <NavLink to="/Papers" className="bar">
-                            <p>Papers</p>
-                        </NavLink>
-                        <NavLink to="/Speakers" className="bar">
-                            <p>Speakers</p>
-                        </NavLink>
-                        {loc.pathname === '/' ? (
-                            <div className="bar">
-                                <Link
-                                    activeClass="active"
-                                    className="bar"
-                                    to="sponsorship"
-                                    spy={true}
-                                    smooth={true}
-                                    duration={500}
-                                >
-                                    Sponsorship
-                                </Link>
-                            </div>
-                        ) : (
-                            <NavLink exact to="/" className="bar">
-                                <p>sponsorship</p>
-                            </NavLink>
-                        )}
-                        <NavLink to="/Contactus" className="bar">
-                            <p>Contact us</p>
-                        </NavLink>
-                        <NavLink to="/Committee" className="bar">
-                            <p>Committee</p>
-                        </NavLink>
-                        <NavLink to="/registration" className="bar">
-                            <p>Registration</p>
-                        </NavLink>
-                        {loc.pathname === '/' ? (
-                            <div className="bar">
-                                <Link
-                                    activeClass="active"
-                                    className="bar"
-                                    to="awards"
-                                    spy={true}
-                                    smooth={true}
-                                    duration={500}
-                                >
-                                    Awards
-                                </Link>
-                            </div>
-                        ) : (
-                            <NavLink exact to="/" className="bar">
-                                <p>Awards</p>
-                            </NavLink>
-                        )}
-                    </div>
-                ) : null}
-                {x >= 500 ? (
-                              <div className="options">
-                              <NavLink exact to="/" className="bar">
-                                <p>Home</p>
-                              </NavLink>
-                              <NavLink to="/Papers" className="bar">
-                                <p>Papers</p>
-                              </NavLink>
-                              <NavLink to="/Speakers" className="bar">
-                                <p>Speakers</p>
-                              </NavLink>
-                              {loc.pathname === '/' ? (
-                                <div className="bar">
-                                  <Link
-                                    activeClass="active"
-                                    className="bar"
-                                    to="sponsorship"
-                                    spy={true}
-                                    smooth={true}
-                                    duration={500}
-                                  >
-                                    Sponsorship
-                                  </Link>
-                                </div>
-                              ) : (
-                                <NavLink exact to="/" className="bar">
-                                  <p>sponsorship</p>
-                                </NavLink>
-                              )}
-                              <NavLink to="/Contactus" className="bar">
-                                <p>Contact us</p>
-                              </NavLink>
-                              <NavLink to="/Committee" className="bar">
-                                <p>Committee</p>
-                              </NavLink>
-                              <NavLink to="/registration" className="bar">
-                                <p>Registration</p>
-                              </NavLink>
-                              {loc.pathname === '/' ? (
-                                <div className="bar">
-                                  <Link
-                                    activeClass="active"
-                                    className="bar"
-                                    to="awards"
-                                    spy={true}
-                                    smooth={true}
-                                    duration={500}
-                                  >
-                                    Awards
-                                  </Link>
-                                </div>
-                              ) : (
-                                <NavLink exact to="/" className="bar">
-                                  <p>Awards</p>
-                                </NavLink>
-                              )}
-                            </div>
-                         
-                  
-                ) : null}
-                <div className="logo">Stanford</div>
-            </div>
-        </>
-    );
+  //   })
+
+  return (
+    <>
+      <div className="nav">
+        {x < 500 ? (
+          <div className={burgerClassName} onClick={toggleMenu}>
+            <Button variant="outlined">
+              <MenuIcon style={{ color: 'white' }} />
+            </Button>
+          </div>
+        ) : null}
+        {showMenu && x < 500 ? (
+          <div className="menu">
+            <NavLink exact to="/" className={loc.pathname==='/'?'active-tab':'bar'}>
+              <p>Home</p>
+            </NavLink>
+            <NavLink to="/Papers" className={loc.pathname==='/Papers'?'active-tab':'bar'}>
+              <p>Papers</p>
+            </NavLink>
+            <NavLink to="/Speakers" className={loc.pathname==='/Speakers'?'active-tab':'bar'}>
+              <p>Speakers</p>
+            </NavLink>
+            {loc.pathname === '/' ? (
+              <div className="bar">
+                <Link
+                  activeClass="active"
+                  className='bar'
+                  to="sponsorship"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Sponsorship
+                </Link>
+              </div>
+            ) : (
+              <NavLink exact to="/" className="bar">
+                <p>sponsorship</p>
+              </NavLink>
+            )}
+            <NavLink to="/Contactus" className={loc.pathname==='/Contactus'?'active-tab':'bar'}>{
+              
+            }
+              <p>Contact us</p>
+            </NavLink>
+            <NavLink to="/Committee" className={loc.pathname==='/Committee'?'active-tab':'bar'}>
+              <p>Committee</p>
+            </NavLink>
+            <NavLink to="/registration" className={loc.pathname==='/registration'?'active-tab':'bar'}>
+              <p>Registration</p>
+            </NavLink>
+            {loc.pathname === '/' ? (
+              <div className="bar">
+                <Link
+                  activeClass="active"
+                  className="bar"
+                  to="awards"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Awards
+                </Link>
+              </div>
+            ) : (
+              <NavLink exact to="/" className="bar">
+                <p>Awards</p>
+              </NavLink>
+            )}
+          </div>
+        ) : null}
+        {x >= 500 ? (
+          <div className="options">
+            <NavLink exact to="/" className={loc.pathname==='/'?'active-tab':'bar'}>
+              <p>Home</p>
+            </NavLink>
+            <NavLink to="/Papers" className={loc.pathname==='/Papers'?'active-tab':'bar'}>
+              <p>Papers</p>
+            </NavLink>
+            <NavLink to="/Speakers" className={loc.pathname==='/Speakers'?'active-tab':'bar'}>
+              <p>Speakers</p>
+            </NavLink>
+            {loc.pathname === '/' ? (
+              <div className="bar">
+                <Link
+                  activeClass="active"
+                  className="bar"
+                  to="sponsorship"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Sponsorship
+                </Link>
+              </div>
+            ) : (
+              <NavLink exact to="/" className="bar">
+                <p>sponsorship</p>
+              </NavLink>
+            )}
+            <NavLink to="/Contactus" className="bar">
+              <p>Contact us</p>
+            </NavLink>
+            <NavLink to="/Committee" className="bar">
+              <p>Committee</p>
+            </NavLink>
+            <NavLink to="/registration" className="bar">
+              <p>Registration</p>
+            </NavLink>
+            {loc.pathname === '/' ? (
+              <div className="bar">
+                <Link
+                  activeClass="active"
+                  className="bar"
+                  to="awards"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Awards
+                </Link>
+              </div>
+            ) : (
+              <NavLink exact to="/" className="bar">
+                <p>Awards</p>
+              </NavLink>
+            )}
+          </div>
+
+
+        ) : null}
+        <div className="logo">Stanford</div>
+      </div>
+    </>
+  );
 };
 
 export default Nav;
